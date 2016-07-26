ba7747f1955d11c7b40700c58d8b9c713dcdad09
James <jlinnega@thoughtworks.com>
2016-07-26 09:14:35 +0100

    Daily forecast and print icon url, (now with icons)

--- weather.py
+++ weather.py
@@ -3,16 +3,17 @@ import os
 
 
 api_key = os.environ['FORECASTIO']
-
 lat = 53.4393315
 lon = -1.9568661
 
-forecast = forecastio.load_forecast(api_key, lat, lon)
 
-byHour = forecast.hourly()
+#-------------------------------------------------------------------------------
+#  M A I N
+#-------------------------------------------------------------------------------
+if __name__ == "__main__":
 
-for hourlyData in byHour.data:
-  print hourlyData.icon
-  print hourlyData.temperature
-
+  forecast = forecastio.load_forecast(api_key, lat, lon)
+  daily = forecast.currently()  
+  url = "./icons/%s.png" % daily.icon
+  print url
 
