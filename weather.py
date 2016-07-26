f5382f6c2928d78900cebbda8e516a8e2e44ffb4
James <jlinnega@thoughtworks.com>
2016-07-26 10:50:10 +0100

    Python imaging

--- weather.py
+++ weather.py
@@ -1,5 +1,9 @@
 import forecastio
 import os
+import sys
+
+from PIL import Image
+from PIL import ImageOps
 
 
 api_key = os.environ['FORECASTIO']
@@ -14,6 +18,10 @@ if __name__ == "__main__":
 
   forecast = forecastio.load_forecast(api_key, lat, lon)
   daily = forecast.currently()  
-  url = "./icons/%s.png" % daily.icon
-  print url
+  file_name = "./icons/%s.png" % daily.icon
+
+  image = Image.open(file_name)
+  image = ImageOps.grayscale(image)
+
+  print file_name
 
