d80688440c4dfe8b773f4365b9b9d73b9ef08e07
James <jlinnega@thoughtworks.com>
2016-07-26 21:57:42 +0100

    Adding in text to rendering:

--- weather.py
+++ weather.py
@@ -8,19 +8,43 @@ from PIL import ImageOps
 from PIL import ImageDraw
 from PIL import ImageFont
 
+from datetime import datetime
+import time
+
 from EPD import EPD
 
-api_key = os.environ['FORECASTIO']
-lat = 53.4393315
-lon = -1.9568661
+possible_fonts = [
+    '/usr/share/fonts/truetype/ttf-dejavu/DejaVuSansMono-Bold.ttf',   # R.Pi
+    '/usr/share/fonts/truetype/freefont/FreeMono.ttf',                # R.Pi
+    '/usr/share/fonts/truetype/LiberationMono-Bold.ttf',              # B.B
+    '/usr/share/fonts/truetype/DejaVuSansMono-Bold.ttf',              # B.B
+    '/usr/share/fonts/TTF/FreeMonoBold.ttf',                          # Arch
+    '/usr/share/fonts/TTF/DejaVuSans-Bold.ttf'                        # Arch
+]
 
 
-#-------------------------------------------------------------------------------
-#  M A I N
-#-------------------------------------------------------------------------------
-if __name__ == "__main__":
+FONT_FILE = ''
+FONT_SIZE  = 30
+
+for f in possible_fonts:
+    if os.path.exists(f):
+        FONT_FILE = f
+        break
+
+if '' == FONT_FILE:
+    raise 'no font file found'
+
+
+def main(argv):
   epd = EPD()
-  epd.clear()
+  display(epd)
+
+
+def display(epd):
+  api_key = os.environ['FORECASTIO']
+
+  lat = 53.4393315
+  lon = -1.9568661
 
   forecast = forecastio.load_forecast(api_key, lat, lon)
   daily = forecast.currently()  
@@ -29,8 +53,27 @@ if __name__ == "__main__":
   image = Image.open(file_name)
   image = ImageOps.grayscale(image)
 
+  draw = ImageDraw.Draw(image)
+
+  font = ImageFont.truetype(FONT_FILE, FONT_SIZE)
+  
+  draw.text((5, 10), 'TEST', fill=BLACK, font=clock_font)
+
   rs = image.resize((epd.width, epd.height))
   bw = rs.convert("1", dither=Image.FLOYDSTEINBERG)
 
+  epd.clear()
   epd.display(bw)
   epd.update()
+
+
+#-------------------------------------------------------------------------------
+#  M A I N
+#-------------------------------------------------------------------------------
+if __name__ == "__main__":
+  try:
+      main(sys.argv[1:])
+  except KeyboardInterrupt:
+      sys.exit('interrupted')
+      pass
+
