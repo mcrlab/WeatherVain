1a500a4be017959a03a58af7d9f64027237bc988
James Linnegar <jlinnega@thoughtworks.com>
2016-07-26 13:46:32 +0000

    Adding EPD and display

--- weather.py
+++ weather.py
@@ -1,10 +1,11 @@
 import forecastio
 import os
 import sys
+import re
 
 from PIL import Image
 from PIL import ImageOps
-
+from EPD import EPD
 
 api_key = os.environ['FORECASTIO']
 lat = 53.4393315
@@ -15,6 +16,8 @@ lon = -1.9568661
 #  M A I N
 #-------------------------------------------------------------------------------
 if __name__ == "__main__":
+  epd = EPD()
+  epd.clear()
 
   forecast = forecastio.load_forecast(api_key, lat, lon)
   daily = forecast.currently()  
@@ -23,5 +26,10 @@ if __name__ == "__main__":
   image = Image.open(file_name)
   image = ImageOps.grayscale(image)
 
+  rs = image.resize((epd.width, epd.height))
+  bw = rs.convert("1", dither=Image.FLOYDSTEINBERG)
+
+  epd.display(bw)
+  epd.update()
   print file_name
 
