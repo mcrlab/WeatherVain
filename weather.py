35a9c14d4bdf00eafb7fd96633901eee35b15e1f
James <jlinnega@thoughtworks.com>
2016-08-09 14:46:59 +0100

    Runs with and without EPD

--- weather.py
+++ weather.py
@@ -8,12 +8,16 @@ from PIL import ImageOps
 from PIL import ImageDraw
 from PIL import ImageFont
 
+from EPD import EPD
+
 from datetime import datetime
 import time
 
-from EPD import EPD
 WHITE = 1
 BLACK = 0
+
+WIDTH = 264
+HEIGHT = 176
 
 api_text_file = open('%s/api.txt' % (os.path.dirname(os.path.realpath(__file__))))
 api_key = api_text_file.read().strip(' \t\n\r')
@@ -25,61 +29,48 @@ if '' == api_key:
 FONT_SIZE = 15
 
 def main(argv):
-  epd = EPD()
-  print epd.size
-  display(epd)
+  forecast = getForecast()
+  canvas = getCanvas(forecast)
+  render(canvas)
 
-
-def getWeatherImage(text):
+def getWeatherIcon(text):
   file_name = "%s/icons/%s.png" % (os.path.dirname(os.path.realpath(__file__)), text)
   print file_name
   image = Image.open(file_name)
   image = ImageOps.grayscale(image)
   return image
 
-def display(epd):
+
+def getForecast():
 
   lat = 53.4393315
   lon = -1.9568661
 
-  print "clearing Screen"
-  epd.clear()
   print "fetching forecast"
 
-  forecast = forecastio.load_forecast(api_key, lat, lon)
+  return forecastio.load_forecast(api_key, lat, lon)
   
-  currently = forecast.currently()
-  byHour = forecast.hourly()
 
-  canvas = Image.new("1", epd.size, WHITE)
+def getCanvas(forecast):
+  currently = forecast.currently()
+
+  canvas = Image.new("1", (264,176), WHITE)
   draw = ImageDraw.Draw(canvas)
   weather_image = currently.icon
+  image = getWeatherIcon(weather_image)
+  icon_x = (264/2) - (image.width / 2);
+  icon_y = 0
+  canvas.paste(image, (icon_x, icon_y))
 
-  image = getWeatherImage(weather_image)
-  icon_x = 0;
-  icon_y = (epd.height / 2)
-  canvas.paste(image, (icon_x, icon_y - (178/2)))
+  return canvas
 
-  font = ImageFont.truetype("%s/Dosis-ExtraBold.ttf" % os.path.dirname(os.path.realpath(__file__)), FONT_SIZE)
-
-  index = 0
-  icon_index = 0
-  for hourlyData in byHour.data:
-    if icon_index < 3:
-      if(hourlyData.icon != weather_image):
-        print hourlyData.time.strftime("%H:%M")
-        weather_image = hourlyData.icon
-        print hourlyData.time.strftime("%Y-%m-%d %H:%M")
-        icon = getWeatherImage(weather_image)
-        icon_rs = icon.resize((60, 60))
-        icon_rs = icon_rs.convert("1", dither=Image.FLOYDSTEINBERG)
-        canvas.paste(icon_rs,((epd.width - 60),(icon_index*60)))
-        icon_index = icon_index + 1
-        draw.text((178, (icon_index * 60)-(30+(FONT_SIZE/2)), "%s" % hourlyData.time.strftime("%h%p"), fill=BLACK, font=font)
-    index = index + 1
-
-  epd.display(canvas)
-  epd.update()
+def render(canvas):
+  try:
+    epd = EPD()
+    epd.display(canvas)
+    epd.update()
+  except IOError:
+    canvas.show()
 
 
 #-------------------------------------------------------------------------------
