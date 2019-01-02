7ab73a0c57d4512971eb65f4019496dc4b07fc23
James Linnegar <jlinnega@thoughtworks.com>
2019-01-02 17:01:26 +0000

    Adding in a text description underneath the image

--- weather.py
+++ weather.py
@@ -19,29 +19,41 @@ GREY = 0.5
 WIDTH = 264
 HEIGHT = 176
 
+FONT_SIZE = 15;
+
+def validateConfig(cfg):
+    pass
+
 def main(argv):
 
   try:
     with open('%s/config.json' % (os.path.dirname(os.path.realpath(__file__)))) as json_data_file:
       cfg = json.load(json_data_file)
+      validateConfig(cfg)
       start(cfg)
-  except IOError:
-    canvas = buildCanvas("fail")
+  except IOError as error:
+    canvas = buildCanvas("fail", "No config file");
     print("No config file found");
-    render(canvas)
+    render(canvas);
+  except ValueError as error:
+    print(error);
+
 
 def  start(cfg):
   try:
     forecast = getForecast(cfg)
+    icon =  forecast['currently']['icon'];
+    summary = forecast['hourly']['summary'];
+
   except requests.ConnectionError as e:
     print("Connection Error");
     forecast = "fail"
 
-  canvas = buildCanvas(forecast)
+  canvas = buildCanvas(icon, summary);
   render(canvas)
 
-def getWeatherIcon(text):
-  file_name = "%s/icons/%s.png" % (os.path.dirname(os.path.realpath(__file__)), text)
+def getWeatherIcon(icon_name):
+  file_name = "%s/icons/%s.png" % (os.path.dirname(os.path.realpath(__file__)), icon_name)
   print(file_name);
   image = Image.open(file_name)
   image = ImageOps.grayscale(image)
@@ -52,18 +64,55 @@ def getForecast(cfg):
 
   print( "fetching forecast" );
   url = 'https://api.darksky.net/forecast/%s/%s,%s' % (cfg['api'], cfg['lat'], cfg['lon'])
-
   r = requests.get(url)
   data = r.json()
-  return data['currently']['icon']
+  return data;
 
-def buildCanvas(forecast):
-  canvas = Image.new("1", (264,176), WHITE)
+def draw_text(text):
+    pass
+
+def buildCanvas(icon_name, message=""):
+  canvas = Image.new("1", (WIDTH,HEIGHT), WHITE)
   draw = ImageDraw.Draw(canvas)
-  image = getWeatherIcon(forecast)
-  icon_x = (264//2) - (178 // 2);
-  icon_y = 0
+  font = ImageFont.truetype("%s/Dosis-ExtraBold.ttf" % os.path.dirname(os.path.realpath(__file__)), FONT_SIZE);
+
+  image = getWeatherIcon(icon_name)
+  icon_x = (WIDTH//2) - (HEIGHT // 2);
+  icon_y = -10;
   canvas.paste(image, (icon_x, icon_y))
+  text_width, text_height = draw.textsize(message, font=font)
+  if(text_width < WIDTH):
+      draw.text(((WIDTH//2) - (text_width // 2),HEIGHT - 30), message, font=font, fill=BLACK)
+  else:
+      line = ""
+      lines = []
+      width_of_line = 0
+      number_of_lines = 0
+
+      for token in message.split():
+          token = token + " ";
+          token_width = font.getsize(token)[0]
+
+          if width_of_line+token_width < WIDTH:
+              line+=token
+              width_of_line+=token_width
+          else:
+              lines.append(line)
+              number_of_lines += 1
+              width_of_line = 0
+              line = ""
+              line+=token
+              width_of_line+=token_width
+      if line:
+          lines.append(line)
+          number_of_lines += 1
+
+      y_text = HEIGHT - 15 - (len(lines) * 15)#
+      # render each sentence
+      for line in lines:
+          width, height = font.getsize(line)
+          draw.text(((WIDTH - width) // 2, y_text), line, font=font, fill=BLACK)
+          y_text += height
 
   return canvas
 
