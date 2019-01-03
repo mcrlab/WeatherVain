5bb5f74c05e450003d46cbd7df810cb9456a0ce1
James Linnegar <jlinnega@thoughtworks.com>
2019-01-03 08:35:34 +0000

    Bytes support for EPD and Python3

--- EPD.py
+++ EPD.py
@@ -130,7 +130,7 @@ to use:
             raise EPDError('image size mismatch')
 
         with open(os.path.join(self._epd_path, 'LE', 'display_inverse'), 'r+b') as f:
-            f.write(image.tostring())
+            f.write(image.tobytes())
 
         if self.auto:
             self.update()
@@ -147,4 +147,4 @@ to use:
 
     def _command(self, c):
         with open(os.path.join(self._epd_path, 'command'), 'wb') as f:
-            f.write(c)
+            f.write(str.encode(c));
